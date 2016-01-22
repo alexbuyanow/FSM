d8f5e20fd7402ff1100f164404c39a546e199c8a
Alexey Buyanow <alexbuyanow@gmail.com>
2016-01-22 23:59:28 +0300

    Refactoring: config strings to constants

--- example/include/config.php
+++ example/include/config.php
@@ -1,60 +1,64 @@
 <?php
 
 use FSM\FSMLocator;
+use FSM\Listener\ListenerManager;
+use FSM\Machine\MachineFactory;
+use FSM\State\StateFactory;
+use FSM\Transition\TransitionFactory;
 
 return [
-    FSMLocator::CONFIG_KEY_OPTIONS => [
+    FSMLocator::CONFIG_KEY_OPTIONS  => [
         FSMLocator::OPTIONS_KEY_RESOLVER  => FSM\Resolver\DefaultResolver::class,
         FSMLocator::OPTIONS_KEY_STRICT    => FSM\Machine\Machine::STRICT_ALL,
     ],
 
     FSMLocator::CONFIG_KEY_MACHINES => [
         'Context' => [
-            FSM\Machine\MachineFactory::CONFIG_KEY_STATES => [
-                'created' => ['type' => $typeRegular],
-                'edited' => ['type' => $typeRegular],
-                'active' => ['type' => $typeRegular],
-                'inactive' => ['type' => $typeRegular],
-                'deleted' => ['type' => $typeRegular],
+            MachineFactory::CONFIG_KEY_STATES       => [
+                'created'   => [StateFactory::CONFIG_KEY_TYPE => $typeRegular],
+                'edited'    => [StateFactory::CONFIG_KEY_TYPE => $typeRegular],
+                'active'    => [StateFactory::CONFIG_KEY_TYPE => $typeRegular],
+                'inactive'  => [StateFactory::CONFIG_KEY_TYPE => $typeRegular],
+                'deleted'   => [StateFactory::CONFIG_KEY_TYPE => $typeRegular],
             ],
 
-            FSM\Machine\MachineFactory::CONFIG_KEY_TRANSITIONS => [
+            MachineFactory::CONFIG_KEY_TRANSITIONS  => [
                 [
-                    'from' => 'created',
-                    'to' => 'edited',
-                    'signal' => 'edit',
+                    TransitionFactory::CONFIG_KEY_STATE_FROM    => 'created',
+                    TransitionFactory::CONFIG_KEY_STATE_TO      => 'edited',
+                    TransitionFactory::CONFIG_KEY_SIGNAL        => 'edit',
                 ],
 
                 [
-                    'from' => 'edited',
-                    'to' => 'active',
-                    'signal' => 'activate',
+                    TransitionFactory::CONFIG_KEY_STATE_FROM    => 'edited',
+                    TransitionFactory::CONFIG_KEY_STATE_TO      => 'active',
+                    TransitionFactory::CONFIG_KEY_SIGNAL        => 'activate',
                 ],
                 [
-                    'from' => 'edited',
-                    'to' => 'inactive',
-                    'signal' => 'deactivate',
-                ],
-
-                [
-                    'from' => 'active',
-                    'to' => 'deleted',
-                    'signal' => 'delete',
-                    'guard' => 'SimpleGuard',
+                    TransitionFactory::CONFIG_KEY_STATE_FROM    => 'edited',
+                    TransitionFactory::CONFIG_KEY_STATE_TO      => 'inactive',
+                    TransitionFactory::CONFIG_KEY_SIGNAL        => 'deactivate',
                 ],
 
                 [
-                    'from' => 'inactive',
-                    'to' => 'deleted',
-                    'signal' => 'delete',
-                    'guard' => 'SimpleGuard',
+                    TransitionFactory::CONFIG_KEY_STATE_FROM    => 'active',
+                    TransitionFactory::CONFIG_KEY_STATE_TO      => 'deleted',
+                    TransitionFactory::CONFIG_KEY_SIGNAL        => 'delete',
+                    TransitionFactory::CONFIG_KEY_GUARD         => 'SimpleGuard',
+                ],
+
+                [
+                    TransitionFactory::CONFIG_KEY_STATE_FROM    => 'inactive',
+                    TransitionFactory::CONFIG_KEY_STATE_TO      => 'deleted',
+                    TransitionFactory::CONFIG_KEY_SIGNAL        => 'delete',
+                    TransitionFactory::CONFIG_KEY_GUARD         => 'SimpleGuard',
                 ],
             ],
 
-            FSM\Machine\MachineFactory::CONFIG_KEY_LISTENERS => [
+            MachineFactory::CONFIG_KEY_LISTENERS    => [
                 [
-                    'event'         => \FSM\Event\EventInterface::MACHINE_REFRESH_POST,
-                    'listener'      => 'SimpleListener',
+                    ListenerManager::CONFIG_KEY_EVENT       => FSM\Event\Event::MACHINE_REFRESH_POST,
+                    ListenerManager::CONFIG_KEY_LISTENER    => SimpleListener::class,
                 ],
             ],
         ],
